a0ffe0bb41a3a8721f81d1784b1445b252da00c5
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:47:58 +0100

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+
+    public String name;
+    public String surname;
+    public String address;
+
+    public Employee(String newEmployeeName, String newEmployeeSurname, String newEmployeeAddress){
+        this.name = newEmployeeName;
+        this.surname = newEmployeeSurname;
+        this.address = newEmployeeAddress;
+    }
+
+    public String getEmployeeDetails(){
+        return "the new employee " + name + " " + surname + " lives in " + address + ".";
+    }
 }
