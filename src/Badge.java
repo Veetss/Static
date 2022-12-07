3bfb7d91142dcc76390c0cae3cf44b1e10c57b23
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:47:37 +0100

    Update Badge.java

--- src/Badge.java
+++ src/Badge.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Badge {
+public class Badge {
+
+    private static int totalNumberOfEmployees;
+    private String badgeIdCode;
+    public Employee employee;
+
+    private static void keepTrackOfEmployeesNumber(){
+        totalNumberOfEmployees++;
+    }
+
+    private String generateBadgeIdCode(){
+        return "***" + employee.name + employee.surname + "***";
+    }
+
+    public void showBadgeDetails(){
+        System.out.println("Tracked employees: " + totalNumberOfEmployees + ", " + employee.getEmployeeDetails() + ". Badge Id: " + badgeIdCode);
+    }
+
+    public Badge(Employee employeeThatNeedsBadge){
+        keepTrackOfEmployeesNumber();
+        this.employee = employeeThatNeedsBadge;
+        this.badgeIdCode = generateBadgeIdCode();
+    }
 }
