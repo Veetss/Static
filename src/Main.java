924777a4578212c3a670ae7ddcf539545cac8a35
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:48:21 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee pino = new Employee("Pino", "Daniele", "Via Napoli Mille Colori, 6");
+        Badge pinoBadge = new Badge(pino);
+
+        pinoBadge.showBadgeDetails();
+
+        Employee gianni = new Employee("Gianni", "Celeste", "Via Povero Gabbiano, 10");
+        Badge gianniBadge = new Badge(gianni);
+
+        gianniBadge.showBadgeDetails();
     }
 }
